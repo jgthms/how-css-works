7fabffe9c68ac7adc796829b79766cfdf1d148b1
Jeremy Thomas <bbxdesign@gmail.com>
2015-04-23 01:58:06 +0100

    The Flow

--- js/site.js
+++ js/site.js
@@ -1,5 +1,18 @@
 jQuery(document).ready(function ($) {
 
+  $('.content h3').each(function() {
+    var $el = $(this);
+    var id = $el.attr('id');
+    if (id) {
+      $el.prepend(
+        $('<a />')
+        .attr('href', '#' + id)
+        .html('#')
+      );
+      return;
+    }
+  });
+
   $(window).load(function() {
 
     $('[data-typer-targets]').typer();
