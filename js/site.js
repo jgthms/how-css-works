62e3d3a7e595f4cbde111a4e7ef9e6bdf542d457
Jeremy Thomas <bbxdesign@gmail.com>
2015-04-28 23:30:14 +0100

    Verdana, white, simple

--- js/site.js
+++ js/site.js
@@ -1,5 +1,35 @@
 jQuery(document).ready(function ($) {
 
+  function R(s) {
+    return R13(R5(s));
+  }
+
+  function R5(s) {
+    var b = [], c, i = s.length, a = '0'.charCodeAt(), z = a + 10;
+    while (i--) { 
+      c = s.charCodeAt(i);
+      if (c >= a && c < z) { b[i] = String.fromCharCode(((c - a + 5) % (10)) + a); }
+      else { b[i] = s.charAt(i); }
+    }
+    return b.join('');
+  }
+
+  function R13(s) {
+    var b = [], c, i = s.length, a = 'a'.charCodeAt(), z = a + 26, A = 'A'.charCodeAt(), Z = A + 26;
+    while (i--) {
+      c = s.charCodeAt(i);
+      if (c >= a && c < z) { b[i] = String.fromCharCode(((c - a + 13) % (26)) + a); }
+      else if (c >= A && c < Z) { b[i] = String.fromCharCode(((c - A + 13) % (26)) + A); }
+      else { b[i] = s.charAt(i); }
+    }
+    return b.join('');
+  }
+  
+  var $c = $('#c');
+  var t = 'znvygb:'
+  var m = 'ookqrfvta@tznvy.pbz';
+  $c.attr('href', R(t+m));
+
   $('.content h3').each(function() {
     var $el = $(this);
     var id = $el.attr('id');
--- resources.md
+++ resources.md
@@ -3,8 +3,16 @@ layout: page
 title: Resources
 ---
 
-This is the base Jekyll theme. You can find out more info about customizing your Jekyll theme, as well as basic Jekyll usage documentation at [jekyllrb.com](http://jekyllrb.com/)
-
-You can find the source code for the Jekyll new theme at: [github.com/jglovier/jekyll-new](https://github.com/jglovier/jekyll-new)
-
-You can find the source code for Jekyll at [github.com/jekyll/jekyll](https://github.com/jekyll/jekyll)
+<ul class="list">
+  {% for resource in site.resources %}
+    <li>
+      <em class="list-info">
+        {{ resource.domain }}
+      </em>
+      <p class="list-title">
+        <a href="{{ resource.url | prepend: site.baseurl }}">{{ resource.title }}</a>
+      </p>
+      <p class="list-subtitle">{{ resource.subtitle }}</p>
+    </li>
+  {% endfor %}
+</ul>
